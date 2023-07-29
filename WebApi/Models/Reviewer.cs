d6d9ce5f21b2f242aa70eb83e0ffc81355e61e65
Tony Gilmore <tgilmore216@gmail.com>
2023-07-29 19:57:42 +0100

    all files added

---
+++ WebApi/Models/Reviewer.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi.Models
+{
+    public class Reviewer
+    {
+        public int Id { get; set; }
+        public string FristName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
